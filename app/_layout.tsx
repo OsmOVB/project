f02b21ed1921e3f1ea7c74e61c16a62a041915db
LuizTakahashii <luiz.takahashi@grupointegrado.br>
2025-04-15 22:14:19 -0300

    fix: mostrar barra de status com tema dinâmico

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,19 @@ import { Slot, useRouter, useSegments } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useAuth } from '../hooks/useAuth';
 import React from 'react';
-import { ThemeProvider } from '../context/ThemeContext';
+import { ThemeProvider, useThemeContext } from '../context/ThemeContext';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet, useColorScheme } from 'react-native';
 
 export default function RootLayout() {
   const { user } = useAuth();
   const router = useRouter();
   const segments = useSegments();
+  const colorScheme = useColorScheme(); // Detecta o modo claro/escuro do sistema
 
   useEffect(() => {
     const inAuthGroup = segments[0] === '(auth)';
+
     if (user && inAuthGroup) {
       router.replace('/(tabs)');
     } else if (!user && !inAuthGroup) {
@@ -25,7 +27,7 @@ export default function RootLayout() {
     <ThemeProvider>
       <SafeAreaProvider>
         <SafeAreaView style={styles.safeArea} edges={['top', 'left', 'right']}>
-          <StatusBar style="inverted" translucent backgroundColor="transparent" />
+          <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} translucent={false} backgroundColor="transparent" />
           <Slot />
         </SafeAreaView>
       </SafeAreaProvider>
@@ -39,4 +41,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
- 
